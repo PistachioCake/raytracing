fbd832dc4d6b1ec04f8394d64feffaa0990369f8
Rushil U <rushil.udani@gmail.com>
2023-11-08 20:54:42 -0600

    Section 4.3: Scenes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+#![feature(allocator_api)]
+
+use std::alloc::Global;
+
 use bumpalo::Bump;
-use rand::{random, thread_rng, Rng};
+use rand::{thread_rng, Rng};
 
 use raytracing::{
     camera::CameraBuilder,
-    hittable::{bvh::BvhNode, Hittable, Sphere},
+    hittable::{bvh::BvhNode, Hittable, HittableList, Sphere},
     material::{Dielectric, Lambertian, Material, Metal},
     texture::{GlobalChecker, SolidColor},
     time_utils::{Linear, Unchanging},
@@ -11,32 +15,82 @@ use raytracing::{
 };
 
 fn main() {
-    let bump = Bump::new();
+    let world = match 2 {
+        1 => random_spheres(),
+        2 => two_spheres(),
+        _ => unimplemented!(),
+    };
+
+    let camera = CameraBuilder::default()
+        .with_aspect_ratio(16. / 9.)
+        .with_image_width(400)
+        .with_samples_per_pixel(100)
+        .with_max_depth(50)
+        .with_vfov(20.)
+        .with_lookfrom(Point::new(13., 2., 3.))
+        .with_lookat(Point::new(0., 0., 0.))
+        .with_vup(Vector::new(0., 1., 0.))
+        .with_defocus_angle(0.6)
+        .with_focus_dist(10.);
+    let camera = camera.build();
+
+    camera.render(world);
+}
+
+fn two_spheres() -> &'static dyn Hittable {
+    fn leak<T>(x: T) -> &'static T {
+        Box::leak(Box::new(x))
+    }
+
+    let mut world = HittableList::with_capacity(2);
+
+    let checker = leak(GlobalChecker::new_colors(
+        0.8,
+        Color::new(0.2, 0.3, 0.1),
+        Color::new(0.9, 0.9, 0.9),
+        Global::default(),
+    ));
+
+    let material = leak(Lambertian { albedo: checker });
+
+    world.add(leak(Sphere::<Unchanging>::new(
+        Point::new(0.0, -10.0, 0.0),
+        10.0,
+        material,
+    )));
+
+    world.add(leak(Sphere::<Unchanging>::new(
+        Point::new(0.0, 10.0, 0.0),
+        10.0,
+        material,
+    )));
+
+    leak(world)
+}
+
+fn random_spheres() -> &'static dyn Hittable {
+    let bump: &_ = Box::leak(Box::new(Bump::new()));
     let mut rng = thread_rng();
 
-    // world
     let mut objects: Vec<&dyn Hittable> = Vec::with_capacity(124);
-
     let ground_material = bump.alloc(Lambertian {
         albedo: bump.alloc(GlobalChecker::new_colors(
             0.32,
             Color::new(0.2, 0.3, 0.1),
             Color::new(0.9, 0.9, 0.9),
-            &bump,
+            bump,
         )),
     });
-
     objects.push(bump.alloc(Sphere::<Unchanging>::new(
         Point::new(0., -1000., 0.),
         1000.,
         ground_material,
     )));
-
     for a in -11..11 {
         for b in -11..11 {
             let (a, b) = (a as f32, b as f32);
             let choose_mat: f32 = rng.gen();
-            let center = Point::new(a + 0.9 * rng.gen::<f32>(), 0.2, b + 0.9 * random::<f32>());
+            let center = Point::new(a + 0.9 * rng.gen::<f32>(), 0.2, b + 0.9 * rng.gen::<f32>());
 
             if (center - Point::new(4., 0.2, 0.)).length() <= 0.9 {
                 continue;
@@ -99,21 +153,7 @@ fn main() {
         }),
     )));
 
-    let world = BvhNode::new(objects, &bump);
+    let world = BvhNode::new(objects, bump);
 
-    // camera
-    let camera = CameraBuilder::default()
-        .with_aspect_ratio(16. / 9.)
-        .with_image_width(400)
-        .with_samples_per_pixel(100)
-        .with_max_depth(50)
-        .with_vfov(20.)
-        .with_lookfrom(Point::new(13., 2., 3.))
-        .with_lookat(Point::new(0., 0., 0.))
-        .with_vup(Vector::new(0., 1., 0.))
-        .with_defocus_angle(0.6)
-        .with_focus_dist(10.);
-    let camera = camera.build();
-
-    camera.render(&world);
+    bump.alloc(world)
 }
